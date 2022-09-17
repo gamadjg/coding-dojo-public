0692574bf121498c735396d6329fec8346fc4682
gamadjg <david@variusit.org>
2022-09-16 21:22:59 -0700

    Assignment, likes, complete

--- web-fundamentals/week-2/likes/script.js
+++ web-fundamentals/week-2/likes/script.js
@@ -1,4 +1,10 @@
-function likeCounter() {
-	let likeCounter = document.querySelector(".count");
-	likeCounter.innerHTML = parseInt(likeCounter.innerHTML) + 1;
+// function likeCounter() {
+// 	let likeCounter = document.querySelector(".count");
+// 	likeCounter.innerHTML = parseInt(likeCounter.innerHTML) + 1;
+// }
+
+function likeCounter(counter) {
+	let likeCounter = document.querySelectorAll(".count");
+	likeCounter[counter].innerHTML = parseInt(likeCounter[counter].innerHTML) + 1;
+	// console.log(likeCounter[counter].innerHTML);
 }
