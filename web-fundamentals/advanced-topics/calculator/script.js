cb1a7be445ba291d280de15e0c893e57c7190b4b
gamadjg <david@variusit.org>
2022-09-23 12:46:09 -0700

    Calculator js calculation logic complete

--- web-fundamentals/advanced-topics/calculator/script.js
+++ web-fundamentals/advanced-topics/calculator/script.js
@@ -1,27 +1,41 @@
 let equation = [];
-let display = document.querySelector("#display");
 
 function press(num) {
-	equation.push(num);
-	updateDisplay(num);
+	if (equation[equation.length - 1] == "/" && num == 0) {
+		alert("Can't divide by zero!");
+	} else {
+		equation.push(num);
+		updateDisplay(num, "value");
+	}
 }
 
 function setOP(op) {
 	if (Number.isInteger(equation[equation.length - 1])) {
 		equation.push(op);
-		updateDisplay(op);
+		updateDisplay(op, "operator");
 	} else {
 		alert("Cant add consecutive operators");
 	}
 }
 
-function updateDisplay(newVal) {
+function updateDisplay(newVal, type) {
 	let display = document.querySelector("#display");
-	display.innerHTML = display.innerHTML + newVal;
+
+	if (type == "value" || type == "operator") {
+		display.innerHTML = display.innerHTML + newVal;
+	} else if (type == "clear") {
+		display.innerHTML = "";
+		equation = [];
+	} else if (type == "result") {
+		display.innerHTML = newVal;
+	}
 }
 
 function clr() {
-	let display = document.querySelector("#display");
-	display.innerHTML = "";
-	equation = [];
+	updateDisplay(0, "clear");
+}
+
+function calculate() {
+	let result = eval(equation.join(""));
+	updateDisplay(result, "result");
 }
