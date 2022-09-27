5d71b0bf35e8baf0cbf8df329ab8878e1b809f66
gamadjg <david@variusit.org>
2022-09-27 12:34:09 -0700

    NOW DONE with nested dictionaries and lists assignment

--- python/week-1/nested-dictionaries-and-lists.py
+++ python/week-1/nested-dictionaries-and-lists.py
@@ -1,3 +1,6 @@
+
+# ---------- PART 1: Update Values in Dictionaries and Lists ----------
+
 x = [ [5,2,3], [10,8,9] ] 
 students = [
     {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -27,3 +30,48 @@ print(sports_directory)
 # 4: Change the value 20 in z to 30 
 z[0]['y'] = 30
 print(z)
+
+print("==============================================")
+# ---------- PART 2: Iterate Through a List of Dictionaries ----------
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+def iterateDictionary(some_list):
+    for item in some_list:
+        dictStr = ''
+        for key in item:
+            dictStr  += key + " - " + item[key]
+            if(key != list(item.keys())[-1]):
+                dictStr += ", "
+        print(dictStr)
+
+iterateDictionary(students)
+
+print("==============================================")
+# ---------- PART 3: Get Values From a List of Dictionaries ----------
+def iterateDictionary2(key_name, some_list):
+    for listItem in some_list:
+        if key_name in listItem:
+            print(listItem[key_name])
+
+iterateDictionary2('last_name', students)
+
+print("==============================================")
+# ---------- PART 4: Iterate Through a Dictionary with List Values ----------
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(some_dict):
+    for entry in list(some_dict):
+        print(str(len(some_dict[entry])) + " " + entry.upper())
+        for item in some_dict[entry]:
+            print(item)
+        print('')
+
+printInfo(dojo)
