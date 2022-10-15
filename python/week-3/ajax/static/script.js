3c0dab2c4dcfce425507f69046466590c5b8cff4
gamadjg <david@variusit.org>
2022-10-15 13:05:05 -0700

    Dojo weather started

--- python/week-3/ajax/static/script.js
+++ python/week-3/ajax/static/script.js
@@ -1,29 +1,31 @@
-const lookup_form = document.querySelector("#lookup_form");
+// const lookup_form = document.querySelector("#lookup_form");
 
-lookup_form.addEventListener("submit", function (event) {
-	event.preventDefault();
-	let user = document.querySelector("#user_name");
-	get_user(user.value);
-	lookup_form.reset();
-});
+// lookup_form.addEventListener("submit", function (event) {
+// 	event.preventDefault();
+// 	let user = document.querySelector("#user_name");
+// 	get_user(user.value);
+// 	lookup_form.reset();
+// });
 
 function get_user(user) {
-	fetch("https://api.github.com/users/" + user)
+	fetch(
+		"http://api.openweathermap.org/data/2.5/forecast/daily?id=524901&APPID=7ed949534dc05bf2e8f15a239c46a3a7"
+	)
 		.then((response) => {
 			if (response.ok) return response.json();
 			throw new Error("Something went wrong");
 		})
 		.then((data) => {
 			console.log(data);
-			document.querySelector("#account").innerHTML = "";
-			let user_name = document.createElement("h1");
-			user_name.innerText = data.login;
+			// document.querySelector("#account").innerHTML = "";
+			// let user_name = document.createElement("h1");
+			// user_name.innerText = data.login;
 
-			let user_img = document.createElement("img");
-			user_img.setAttribute("src", data.avatar_url);
+			// let user_img = document.createElement("img");
+			// user_img.setAttribute("src", data.avatar_url);
 
-			document.querySelector("#account").appendChild(user_name);
-			document.querySelector("#account").appendChild(user_img);
+			// document.querySelector("#account").appendChild(user_name);
+			// document.querySelector("#account").appendChild(user_img);
 		})
 		.catch((error) => console.log(error));
 }
