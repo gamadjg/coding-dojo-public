cda8aaeb05b3323ed2751759505de688594a7ecd
gamadjg <david@variusit.org>
2022-10-25 15:06:46 -0700

    Assignment, time to duel, complete

--- mern/week-1/super_ninja.js
+++ mern/week-1/super_ninja.js
@@ -5,17 +5,14 @@ class Ninja {
 		this.speed = 3;
 		this.strength = 3;
 	}
-
 	sayName() {
 		console.log("Ninja name: " + this.name);
 	}
-
 	showStats() {
 		console.log(
 			`Name: ${this.name}\nHealth: ${this.health}\nSpeed: ${this.speed}\nStrength: ${this.strength}`
 		);
 	}
-
 	drinkSake() {
 		this.health += 10;
 	}
@@ -28,7 +25,6 @@ class Sensei extends Ninja {
 		this.strength = 10;
 		this.wisdom = 10;
 	}
-
 	speakWisdom() {
 		super.drinkSake();
 		console.log("Turtles, never liked em.");
