15cac02a599ae1c65bb09b66bfecc213b3cd3f16
gamadjg <david@variusit.org>
2022-10-25 12:14:38 -0700

    Completed super ninja assignment

---
+++ mern/week-1/super_ninja.js
@@ -0,0 +1,40 @@
+class Ninja {
+	constructor(name, health) {
+		this.name = name;
+		this.health = health;
+		this.speed = 3;
+		this.strength = 3;
+	}
+
+	sayName() {
+		console.log("Ninja name: " + this.name);
+	}
+
+	showStats() {
+		console.log(
+			`Name: ${this.name}\nHealth: ${this.health}\nSpeed: ${this.speed}\nStrength: ${this.strength}`
+		);
+	}
+
+	drinkSake() {
+		this.health += 10;
+	}
+}
+
+class Sensei extends Ninja {
+	constructor(name, health) {
+		super(name, health);
+		this.speed = 10;
+		this.strength = 10;
+		this.wisdom = 10;
+	}
+
+	speakWisdom() {
+		super.drinkSake();
+		console.log("Turtles, never liked em.");
+	}
+}
+
+const david = new Sensei("David", 200);
+david.speakWisdom();
+david.showStats();
