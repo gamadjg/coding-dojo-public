22af2f367d96a6c0c6a29cabae6bf44d26f97cc5
gamadjg <david@variusit.org>
2022-10-31 17:12:07 -0700

    checkbox and deleting tasks is now working

--- mern/week-2/todo_list/src/App.js
+++ mern/week-2/todo_list/src/App.js
@@ -5,13 +5,9 @@ import TaskList from "./components/task_list";
 
 function App() {
 	const [task, setTask] = useState("");
-	// const [taskList, setTaskList] = useState([]);
 
 	const handleNewTask = (newTask) => {
-		// setTaskList([...taskList, { value: newTask, complete: 0 }]);
-		// console.log("Task List: " + taskList);
 		setTask(newTask);
-		// console.log("New task: " + newTask);
 	};
 
 	return (
