5dac41e311e7db2ad420590daefb4ebd91de101c
gamadjg <david@variusit.org>
2022-10-31 17:12:07 -0700

    checkbox and deleting tasks is now working

--- mern/week-2/todo_list/src/components/task_list.jsx
+++ mern/week-2/todo_list/src/components/task_list.jsx
@@ -2,6 +2,7 @@ import React, { useReducer, useEffect } from "react";
 const initialState = [];
 
 const reducer = (state, action) => {
+	const newState = [...state];
 	switch (action.type) {
 		case "newTask":
 			if (action.value !== "") {
@@ -9,8 +10,22 @@ const reducer = (state, action) => {
 			}
 			return [];
 		// if complete checkbox, set item as complete
-		case "completeTask":
-			break;
+		case "checkTask":
+			let task = document.getElementById(action.id);
+
+			if (newState[action.id].complete === 0) {
+				newState[action.id].complete = 1;
+				task.classList.replace("row", "rowChecked");
+			} else {
+				newState[action.id].complete = 0;
+				task.classList.replace("rowChecked", "row");
+			}
+			return newState;
+		case "deleteTask":
+			console.log(action.id);
+			newState.splice(action.id, 1);
+			return newState;
+
 		default:
 			return [];
 	}
@@ -20,37 +35,25 @@ const ItemList = (props) => {
 	const [state, dispatch] = useReducer(reducer, initialState);
 
 	useEffect(() => {
-		console.log("useEffect, new task: " + props.newTask);
 		dispatch({
 			type: "newTask",
 			value: props.newTask,
 		});
 	}, [props.newTask]);
-	// const createNewTask = (props) => {
-	// 	dispatch({
-	// 		type: "newTask",
-	// 		value: props.task,
-	// 	});
-	// };
-
-	// const taskList = (task) {
-	// 	dispatch({
-	// 		type: "newTask",
-	// 		value: task,
-	// 	});
-	// };
-
-	// const handleChange = (e) => {
-	// 	// dispatch({
-	// 	// 	type: id, // firstname
-	// 	// 	value: value, // David
-	// 	// });
-	// };
 
 	const checkTask = (e) => {
-		console.log(e.target.id);
-		console.log(e.target.name);
-		// document.getElementById(e.)
+		dispatch({
+			type: "checkTask",
+			id: e.target.value,
+		});
+	};
+
+	const deleteTask = (e) => {
+		console.log(e.target.value);
+		dispatch({
+			type: "deleteTask",
+			id: e.target.value,
+		});
 	};
 
 	return (
@@ -64,7 +67,7 @@ const ItemList = (props) => {
 			<tbody>
 				{state.map((task, id) => {
 					return (
-						<tr key={id}>
+						<tr key={id} id={id} className="row">
 							<td>
 								<p className={`task-${id}`}>{task.value}</p>
 							</td>
@@ -76,19 +79,16 @@ const ItemList = (props) => {
 									value={id}
 								/>
 							</td>
+							<td>
+								<button onClick={deleteTask} value={id}>
+									DELETE
+								</button>
+							</td>
 						</tr>
 					);
 				})}
 			</tbody>
 		</table>
-
-		// <div className="main">
-		// 	<ul>
-		// 		{state.map((task, id) => {
-		// 			return <li key={id}>{task.value}</li>;
-		// 		})}
-		// 	</ul>
-		// </div>
 	);
 };
 
