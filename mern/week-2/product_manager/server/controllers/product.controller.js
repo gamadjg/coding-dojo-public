c18beb2270edd1beb2cd906823efa2c1fbc4b25f
gamadjg <david@variusit.org>
2022-11-07 12:35:42 -0800

    api call to get all products working

--- mern/week-2/product_manager/server/controllers/product.controller.js
+++ mern/week-2/product_manager/server/controllers/product.controller.js
@@ -16,3 +16,9 @@ module.exports.createProduct = (req, res) => {
 		.then((product) => res.json(product))
 		.catch((err) => res.json(err));
 };
+
+module.exports.getProducts = (req, res) => {
+	Product.find()
+		.then((allProducts) => res.json({ Products: allProducts }))
+		.catch((err) => res.json({ message: "Something went wrong", error: err }));
+};
--- mern/week-2/product_manager/server/routes/product.routes.js
+++ mern/week-2/product_manager/server/routes/product.routes.js
@@ -2,5 +2,6 @@ const ProductController = require("../controllers/product.controller");
 
 module.exports = function (app) {
 	app.get("/api", ProductController.index);
+	app.get("/api/products", ProductController.getProducts);
 	app.post("/api/product/create", ProductController.createProduct);
 };
