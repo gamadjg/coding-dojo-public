1af019428b73dc30d5159e20707e46a8f7b5c03b
gamadjg <gamadavid36@gmail.com>
2022-11-08 12:30:23 -0800

    author routes complete

--- mern/week-3/authors/server/controllers/authors.controller.js
+++ mern/week-3/authors/server/controllers/authors.controller.js
@@ -1,4 +1,4 @@
-const { Product } = require("../models/product.model");
+const { Author } = require("../models/Authors.model");
 
 module.exports.index = (req, res) => {
 	res.json({
@@ -7,40 +7,42 @@ module.exports.index = (req, res) => {
 };
 
 module.exports.getAll = (req, res) => {
-	Product.find()
-		.then((allProducts) => res.json(allProducts))
-		.catch((err) => res.json({ message: "Something went wrong", error: err }));
+	Author.find()
+		.then((allAuthors) => res.json(allAuthors))
+		.catch((err) =>
+			res.json({ message: "ERROR, GET: Cannot get all authors.", error: err })
+		);
 };
 
 module.exports.getOne = (req, res) => {
-	Product.findOne({ _id: req.params.id })
-		.then((product) => res.json({ product: product }))
+	Author.findOne({ _id: req.params.id })
+		.then((author) => res.json({ author: author }))
 		.catch((err) => res.json({ message: "Something went wrong", error: err }));
 };
 
 module.exports.create = (req, res) => {
-	const { title, price, description } = req.body;
-	Product.create({
-		title,
-		price,
-		description,
+	const { name } = req.body;
+	Author.create({
+		name,
 	})
-		.then((product) => res.json(product))
-		.catch((err) => res.json(err));
-};
-
-module.exports.update = (req, res) => {
-	Product.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
-		.then((updatedProduct) => res.json({ product: updatedProduct }))
+		.then((author) => res.json(author))
 		.catch((err) =>
-			res.json({ message: "ERROR: Controller update product", error: err })
+			res.json({ message: "ERROR, POST: Cannot create author.", error: err })
 		);
 };
 
-module.exports.delete = (req, res) => {
-	Product.deleteOne({ _id: req.params.id })
-		.then((result) => res.json({ result: result }))
-		.catch((err) =>
-			res.json({ message: "ERROR: Controller delete product", error: err })
-		);
-};
+// module.exports.update = (req, res) => {
+// 	Author.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+// 		.then((updatedAuthor) => res.json({ author: updatedAuthor }))
+// 		.catch((err) =>
+// 			res.json({ message: "ERROR: Controller update author", error: err })
+// 		);
+// };
+
+// module.exports.delete = (req, res) => {
+// 	Author.deleteOne({ _id: req.params.id })
+// 		.then((result) => res.json({ result: result }))
+// 		.catch((err) =>
+// 			res.json({ message: "ERROR: Controller delete author", error: err })
+// 		);
+// };
--- mern/week-3/authors/server/routes/authors.routes.js
+++ mern/week-3/authors/server/routes/authors.routes.js
@@ -1,10 +1,16 @@
 const AuthorController = require("../controllers/author.controller");
 
 module.exports = function (app) {
+	// INDEX
 	app.get("/api", AuthorController.index);
-	// app.get("/api/authors", AuthorController.getAll);
-	// app.get("/api/authors/:id", AuthorController.getOne);
-	// app.post("/api/authors/create", AuthorController.create);
+	// GET ALL
+	app.get("/api/authors", AuthorController.getAll);
+	// GET ONE
+	app.get("/api/authors/:id", AuthorController.getOne);
+	// POST
+	app.post("/api/authors/create", AuthorController.create);
+	// PUT ONE
 	// app.put("/api/authors/:id/edit", AuthorController.update);
+	// DELETE ONE
 	// app.delete("/api/authors/:id/delete", AuthorController.delete);
 };
